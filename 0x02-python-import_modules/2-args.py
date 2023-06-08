000132e5981716bac757f0d5308046715c306457
koechMR <briankipkoech328@gmail.com>
2023-06-08 16:40:57 +0300

    Function that print numbers and list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    arguments = sys.argv[1:]
 
-    a = len(arguments)
+    a = len(sys.argv)-1
     if a == 0:
         print("0 arguments.")
     elif a == 1:
         print("1 arguments.")
     else:
         print("{} arguments:".format(a))
-    for i, argument in enumerate(arguments, start=1):
-        print("{}: {}".format(i, argument))
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg))
