b46c5449f7541370786b2a4837094f4619364a41
koechMR <briankipkoech328@gmail.com>
2023-06-08 20:03:47 +0300

    a  program that prints all the names by the compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import importlib
-    hidden_4 = importlib.import_module('hidden_4')
-    for name in sorted(dir(hidden_4)):
-        if not name.startswith('__'):
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
             print(name)
