f6d3cb526d0c0bf1637051a02ae226966f55d967
koechMR <briankipkoech328@gmail.com>
2023-06-08 19:18:27 +0300

    adding infinite numbers

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import sys
     result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+    for arg in sys.argv[1:]:
+        result += int(arg)
+    print('{:d}'.format(result))
