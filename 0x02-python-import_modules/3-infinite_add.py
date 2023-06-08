6a384bf616c3233d22f4e08e57dd03250349f101
koechMR <briankipkoech328@gmail.com>
2023-06-08 18:51:52 +0300

    infinate addition of all arguments being passed on the command line

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    arguments = sys.argv[1:]
-    result = sum(int(arg) for arg in arguments)
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
     print(result)
