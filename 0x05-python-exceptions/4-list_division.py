95a7f13455ea2f0a7b132d5d726e4773fb723445
koechMR <briankipkoech328@gmail.com>
2023-06-26 12:17:08 +0300

    function that divides elements by elements

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,8 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             quotient = 0
             if i < len(my_list_1) and i < len(my_list_2):
-                if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
+                if isinstance(my_list_1[i], (int, float))
+                and isinstance(my_list_2[i], (int, float)):
                     if my_list_2[i] != 0:
                         quotient = my_list_1[i] / my_list_2[i]
                     else:
