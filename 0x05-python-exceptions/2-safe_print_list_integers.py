c07378600400e64b9cb95dc5d17b88bc701c4db8
koechMR <briankipkoech328@gmail.com>
2023-06-27 08:07:13 +0300

    fixing len

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,8 +2,8 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
-        for i in range(x):
-            if i >= len(my_list):
+        for i in my_list:
+            if count >= x:
                 break
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
@@ -11,4 +11,3 @@ def safe_print_list_integers(my_list=[], x=0):
         print()
     except (TypeError, IndexError):
         return False
-    return count
