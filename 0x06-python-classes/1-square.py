e062ba5211ec872cc5db2a2e92c58b8d04a5b8b0
koechMR <122898923+koechMR@users.noreply.github.com>
2023-06-27 13:08:39 +0300

    Update 1-square.py
    define class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+
+"""Define class square"""
+
+
 class Square:
+    """Square"""
+    
     def __init__(self, size):
+        """Init a new
+
+        Args:
+            size (int): size"""
         self.__size = size
