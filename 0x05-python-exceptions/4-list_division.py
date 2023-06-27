7371a65687bc1546299a347be451eca6e4e2bbf3
koechMR <briankipkoech328@gmail.com>
2023-06-27 08:16:28 +0300

    fixing version

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,8 +5,8 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             quotient = 0
             if i < len(my_list_1) and i < len(my_list_2):
-                if isinstance(my_list_1[i], (int, float))
-                and isinstance(my_list_2[i], (int, float)):
+                if isinstance(my_list_1[i], (int, float)) and \
+                        isinstance(my_list_2[i], (int, float)):
                     if my_list_2[i] != 0:
                         quotient = my_list_1[i] / my_list_2[i]
                     else:
@@ -19,4 +19,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             new_list.append(0)
+        finally:
+            pass
     return new_list
