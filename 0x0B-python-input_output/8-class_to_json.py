548eecb4638079ea8e229a06708b9fecf504b083
koechMR <briankipkoech328@gmail.com>
2023-07-11 16:30:13 +0300

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,7 +3,7 @@
 
 
 def class_to_json(obj):
-    """Return the dictionary represntation 
+    """Return the dictionary represntation
     of a simple data structure
     """
     return obj.__dict__
