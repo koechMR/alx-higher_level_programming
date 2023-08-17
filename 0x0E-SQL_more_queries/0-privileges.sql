30d9e61923923480ba211e8d233b51c9b6e5beaf
koechMR <briankipkoech328@gmail.com>
2023-08-17 11:06:26 +0300

    new

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- lists all privileges of the users  on your server
-
+-- lists all privileges of the users  on your server that will be shown in the server
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
