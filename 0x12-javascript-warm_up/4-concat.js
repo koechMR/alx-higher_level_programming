d0f46628afcd6612039a4c8073555f93c4b39bcb
koechMR <briankipkoech328@gmail.com>
2023-09-11 16:23:28 +0300

    script that prints arguments passed to it

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else if (process.argv[2] !== undefined) {
+  console.log(process.argv[2] + ' is undefined');
 } else {
   console.log('undefined is undefined');
 }
