4abdec5eaea5edcff29fe20dfb4af21504e85190
koechMR <briankipkoech328@gmail.com>
2023-09-11 16:16:54 +0300

    script that prints arguments passed to it

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,5 +2,5 @@
 if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
 } else {
-  console.log('two arguments are required');
+  console.log('undefined is undefined');
 }
