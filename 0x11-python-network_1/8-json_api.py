cd7d46f758074ae6a5f6e9aab45ddc36262ddfe3
koechMR <briankipkoech328@gmail.com>
2023-09-30 06:42:28 +0300

    new post request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that sends a POST request"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": argv[1][0] if len(argv) > 1 else ""}
+    response = requests.post(url,
+                             data=data)
+
+    try:
+        y = response.json()
+        if not y:
+            print("No result")
+        else:
+            print("[{}] {}".format(y.get("id"),
+                                   y.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
